61e51c2dd56d2b8a26bfc242e9f1508761ce5252
StolenRuby <ruby@mobiusgrid.us>
2020-07-28 16:09:43 +0100

    Renamed Solution to Consortium

--- runprebuild.bat
+++ runprebuild.bat
@@ -55,9 +55,9 @@ goto :done
     @echo Found msbuild at %ValueValue%
     @echo Creating compile.bat
 rem To compile in debug mode
-    @echo %ValueValue% opensim.sln > compile.bat
+    @echo %ValueValue% Consortium.sln > compile.bat
 rem To compile in release mode comment line (add rem to start) above and uncomment next (remove rem)
-rem @echo %ValueValue% /P:Config=Release opensim.sln > compile.bat
+rem @echo %ValueValue% /P:Config=Release Consortium.sln > compile.bat
 :done
 if exist "bin\addin-db-002" (
 	del /F/Q/S bin\addin-db-002 > NUL
